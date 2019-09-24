582bbab0bb2604bd0489cd4c89b66fc906df719b
adeshkadambi <adeshkadambi@gmail.com>
2019-09-24 09:36:32 -0400

    adding new links

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
-  }]
-  
+  },
+{
+    id: 'link-1',
+    url: 'www.google.ca',
+    description: 'Google Search'
+}]
+
+let idCount = links.length
+
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
+  },
 }
+
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
+    typeDefs: './src/schema.graphql',
+    resolvers,
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
